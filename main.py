9520da6e787d932bd1ffa2122df6999c93a4a3a2
Bogdan <myakinb@mail.ru>
2022-12-05 20:27:16 +0300

    First commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from PyQt5 import QtWidgets
+from interface import Ui_MainWindow
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    ui.connections()
+    MainWindow.show()
+    sys.exit(app.exec_())
---
+++ processor.py
@@ -0,0 +1,31 @@
+from PyQt5.QtWidgets import QFileDialog, QDialog
+from PyQt5 import QtCore, QtGui, QtWidgets
+import librosa
+
+class Processor(object):
+    def __init__(self):
+        self.path = ""
+
+    def open_file(self, label, btn):
+        file_open = QDialog()
+        self.path = (QFileDialog\
+                     .getOpenFileName(file_open, 'Open Audio File', '/home/layv/Desktop/termpaper',
+                                'MP3 File (*.mp3);;WAV File (*.wav)'))[0]
+        if self.path != "":
+            index = len(self.path) - self.path[::-1].index('/')
+            label.setText(self.path[index:-4])
+            btn.setEnabled(True)
+
+    def analyse(self, bpm_label, key_label):
+        font = QtGui.QFont()
+        font.setFamily("Bahnschrift")
+        font.setPointSize(16)
+        dialog = QDialog()
+        dialog.resize(400, 200)
+        d_lbl = QtWidgets.QLabel('Soon', dialog)
+        d_lbl.setAlignment(QtCore.Qt.AlignCenter)
+        d_lbl.setFont(font)
+        d_lbl.move(100, 50)
+        dialog.setWindowTitle("Dialog")
+        dialog.exec_()
+
